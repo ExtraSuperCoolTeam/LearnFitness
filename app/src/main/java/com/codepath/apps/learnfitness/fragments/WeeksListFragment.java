0027a556575e4c92e51528410a2acb1612911b52
Dillon McCoy <dillon@tellapart.com>
2016-03-11 16:45:23 -0800

    Fix (I think) the problem where the first page doesn't load

--- app/src/main/java/com/codepath/apps/learnfitness/fragments/WeeksListFragment.java
+++ app/src/main/java/com/codepath/apps/learnfitness/fragments/WeeksListFragment.java
@@ -33,6 +33,7 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class WeeksListFragment extends Fragment {
+    private static final String TAG = "WeeksListFragment";
 
     Subscription subscription;
     private LessonsAdapter mAdapter;
@@ -115,31 +116,33 @@ public class WeeksListFragment extends Fragment {
         layoutManager = new LinearLayoutManager(getActivity());
         rvLessons.setLayoutManager(layoutManager);
 
-        Observable<Lesson> call = MediaStoreService.contentStore.fetchContent();
+        final Observable<Lesson> call = MediaStoreService.contentStore.fetchContent();
         subscription = call
             .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Lesson>() {
                     @Override
                     public void onCompleted() {
-                        Log.i("WeeksListFragment", "Api call success");
+                        Log.i(TAG, "Api call success");
                     }
 
                     @Override
                     public void onError(Throwable e) {
                         // cast to retrofit.HttpException to get the response code
-                        Log.i("WeeksListFragment", "in error");
-                        Log.i("WeeksListFragment", e.toString());
+                        Log.i(TAG, "in error");
+                        Log.i(TAG, e.toString());
 
                         if (e instanceof HttpException) {
                             HttpException response = (HttpException) e;
                             int code = response.code();
                         }
+
+                        call.retry();
                     }
 
                     @Override
                     public void onNext(Lesson lesson) {
                         mWeeks.addAll(lesson.getWeeks());
-                        Log.i("WeeksListFragment", lesson.getTitle());
+                        Log.i(TAG, lesson.getTitle());
 
                         SharedPreferences sharedPreferences =
                                 getActivity().getSharedPreferences(LessonListActivity.MY_SHARED_PREFS,
