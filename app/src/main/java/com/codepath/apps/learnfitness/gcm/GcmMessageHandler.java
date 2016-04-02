0269ae05f1997214f985624e71ec2f0ff20bdd0f
Shrikant Pandhare <shrikant0013@gmail.com>
2016-04-01 23:45:12 -0700

    gcm click to feedback details

--- app/src/main/java/com/codepath/apps/learnfitness/gcm/GcmMessageHandler.java
+++ app/src/main/java/com/codepath/apps/learnfitness/gcm/GcmMessageHandler.java
@@ -1,11 +1,17 @@
 package com.codepath.apps.learnfitness.gcm;
 
 import com.google.android.gms.gcm.GcmListenerService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import com.codepath.apps.learnfitness.R;
+import com.codepath.apps.learnfitness.activities.LessonListActivity;
+import com.codepath.apps.learnfitness.models.MyFormMessage;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -24,15 +30,35 @@ public class GcmMessageHandler extends GcmListenerService {
         String message = data.getString("body");
         String title = data.getString("title");
 
-        createNotification(title, message);
+        String formMessageString = data.getString("formMessage");
+        Log.i(TAG, "formMessage str: " + formMessageString);
+
+        Gson gson = new GsonBuilder().serializeNulls().create();
+        MyFormMessage myFormMessage =  gson.fromJson(formMessageString, MyFormMessage.class);
+        Log.i(TAG, "formMessage: " + myFormMessage.getMessage());
+        createNotification(title, message, myFormMessage);
     }
 
     // Creates notification based on title and body received
-    private void createNotification(String title, String body) {
+    private void createNotification(String title, String body, MyFormMessage myFormMessage) {
         Context context = getBaseContext();
+
+        Intent intent = new Intent(this, LessonListActivity.class);
+        intent.putExtra("formMessage", myFormMessage);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        Log.i(TAG, myFormMessage.getId());
+         // Next, let's turn this into a PendingIntent using
+
+        int requestID = (int) System.currentTimeMillis(); //unique requestID to differentiate between various notification with same NotifId
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT; // cancel old intent and create new one
+        PendingIntent pIntent = PendingIntent.getActivity(this, requestID, intent, flags);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.mipmap.ic_launcher).setContentTitle(title)
-                .setContentText(body);
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pIntent);
+
         NotificationManager mNotificationManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(MESSAGE_NOTIFICATION_ID, mBuilder.build());
--- app/src/main/java/com/codepath/apps/learnfitness/models/TrainerReply.java
+++ app/src/main/java/com/codepath/apps/learnfitness/models/TrainerReply.java
@@ -1,9 +1,12 @@
 package com.codepath.apps.learnfitness.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by spandhare on 3/20/16.
  */
-public class TrainerReply {
+public class TrainerReply implements Parcelable {
     FormMessageReply formMessageReply;
     Trainer trainer;
 
@@ -22,4 +25,34 @@ public class TrainerReply {
     public void setTrainer(Trainer trainer) {
         this.trainer = trainer;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.formMessageReply, flags);
+        dest.writeParcelable(this.trainer, 0);
+    }
+
+    public TrainerReply() {
+    }
+
+    protected TrainerReply(Parcel in) {
+        this.formMessageReply = in.readParcelable(FormMessageReply.class.getClassLoader());
+        this.trainer = in.readParcelable(Trainer.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<TrainerReply> CREATOR = new Parcelable.Creator<TrainerReply>() {
+        public TrainerReply createFromParcel(Parcel source) {
+            return new TrainerReply(source);
+        }
+
+        public TrainerReply[] newArray(int size) {
+            return new TrainerReply[size];
+        }
+    };
 }
